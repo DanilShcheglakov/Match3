2484c5d77ee4f1ddfb29a7b01fbe1b3f0c03b781
Danil <123818857+DanilShcheglakov@users.noreply.github.com>
2026-08-21 15:02:26 +0300

    Initial commit

---
+++ Assets/Scripts/Game/Tiles/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Tiles
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        public TileConfig TileConfig { get; private set; }
+        public bool IsInteractable { get; private set; }
+        public bool IsMatched { get; private set; }
+
+        public void SetTileConfig(TileConfig config)
+        {
+            TileConfig = config;
+            IsInteractable = config.IsInteractable;
+            IsMatched = false;
+            GetComponent<SpriteRenderer>().sprite = config.Sprite;
+        }
+
+        public bool Setmatched(bool value) => IsMatched = value;
+    }
+}
